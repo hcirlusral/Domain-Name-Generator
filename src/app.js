e138dbe826e259a108afe6743cbbb78a6ad3af18
hcirlusral <giulianagamba97@gmail.com>
2023-02-16 00:03:01 +0000

    first and final commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainName = [];
+  let pronoun = ["the", "our", "her"];
+  let adj = ["aggressive", "proud", "colossal", "silly"];
+  let noun = ["pizza", "insect", "telephone", "ghost"];
+  let domain = [".com", ".net", ".org"];
+
+  for (let x of pronoun) {
+    for (let y of adj) {
+      for (let z of noun) {
+        for (let a of domain) {
+          domainName.push(x + y + z + a);
+        }
+      }
+    }
+  }
+  const element = document.querySelector("#domains");
+  element.innerText = domainName.join(", ");
 };
